b1b9d359d13b6301c2c2aff8a999176d1d5a6216
Dean Gilewicz <gilewiczd@gmail.com>
2014-09-30 15:39:29 -0400

    calculation functionality updated

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,35 @@
-var button = document.getElementById('calculateBtn');
+var add = document.getElementById('addBtn');
+var subtract = document.getElementById('subtractBtn');
+var multiply = document.getElementById('multiplyBtn');
+var divide = document.getElementById('divideBtn');
 var num1 = document.getElementById('num_one');
 var num2 = document.getElementById('num_two');
 var resultArea = document.getElementById('answer');
 
-button.onclick = function () {
+add.onclick = function () {
 
   var answer = Number(num1.value) + Number(num2.value);
+  resultArea.innerHTML = answer;
+
+};
+
+subtract.onclick = function () {
+
+  var answer = Number(num1.value) - Number(num2.value);
+  resultArea.innerHTML = answer;
+
+};
+
+multiply.onclick = function () {
+
+  var answer = Number(num1.value) * Number(num2.value);
+  resultArea.innerHTML = answer;
+
+};
+
+divide.onclick = function () {
+
+  var answer = Number(num1.value) / Number(num2.value);
   resultArea.innerHTML = answer;
 
 };
